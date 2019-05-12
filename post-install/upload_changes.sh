d6ec7a1ba31127ff4c66e18e7306eaaba4cdbfa9
Tetiana Kurinenko <tetiana.kurinenko@playtech.com>
2019-05-12 14:32:58 +0000

    Update upload_changes.sh

--- post-install/upload_changes.sh
+++ post-install/upload_changes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-master_node="sky-ambari-privil-01.skygue.ptec"
-cluster_name="sky_prod_kafka"
+master_node="mtabet-ambari-privil-01.maltabet.ptec"
+cluster_name="maltabet_kafka"
 ta_ims_pkey="/var/lib/jenkins/ims_ta_keys/ta_ims.ci_prod"
 
 scp -i ${ta_ims_pkey} ams-hbase-log4j.json ta_ims.ci@${master_node}:/tmp/
